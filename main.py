dfb08513c66d9579dc668b4273fe9aa6482c6319
Andrew Burns <andrewburns43214@gmail.com>
2026-01-26 16:56:54 -0500

    metrics: expose net_edge via alias from last_net_edge in row_state

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f426fe22e283a776a7c9ac5f17e217a68e970c9b7b6ba3bef625de03ff055780
-size 186269
+oid sha256:b7ed21ae39bf6beded50aff5eed52f93b6bf0d9ee6f1a787f905c69a3f6d7300
+size 192757
