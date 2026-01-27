ee2edbae1c5909072af9835105d5c4bd1224c25b
Andrew Burns <andrewburns43214@gmail.com>
2026-01-26 22:29:22 -0500

    UI: group-safe table sorting only (GAME+SIDE rows); no logic/metrics changes

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f95e0427ca20456ac25c55aef37bfa6fe33608957cf445a8b869301db37bbf7
-size 198372
+oid sha256:2abcc19f36516408e21ce165d391fcd4bd8d808028c5c4e1f5965d387be7c8d1
+size 198988
