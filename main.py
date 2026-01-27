42c494414d660a6e0d234a468b10fb30849d8669
Andrew Burns <andrewburns43214@gmail.com>
2026-01-26 20:30:15 -0500

    metrics: persist last/peak score and last_decision in row_state

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7ed21ae39bf6beded50aff5eed52f93b6bf0d9ee6f1a787f905c69a3f6d7300
-size 192757
+oid sha256:a2b8f8b9c179e416d076760724a4fe3b232f9da125d1612e758832dbf904991b
+size 194275
