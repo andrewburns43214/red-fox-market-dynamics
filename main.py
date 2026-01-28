1bb444818bfc5557ba499da4070903b61a75b87f
Andrew Burns <andrewburns43214@gmail.com>
2026-01-27 22:56:30 -0500

    chore(v1.1): disable dead dashboard gating-cols writer; keep only always-run metrics writer

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b32e82bf4d6c0bb7ffc7d75c8900a4646754124696467892b16f7a52b50fad3
-size 219488
+oid sha256:af5cf60732b3fce0ce17ae4fdc861d2a4a8c38fc57afe9e1a3a593382c9c9654
+size 219606
