485bd6bbbc0df2b1cefd91b458fe5a9b57a11a20
Andrew Burns <andrewburns43214@gmail.com>
2026-01-27 20:42:28 -0500

    metrics: Step 1E emit STRONG72_ENTER/EXIT before METRICS_DEBUG gate

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49ec5caf95ee58ff172e63d1506f0dce70c1cffd78c37a686f910059f13bcc75
-size 208820
+oid sha256:53fc3de02b02ce587d435bd8f56fbfd475ad85df29d85dff9f14e0b5d3aad17a
+size 210122
