d951d849e6c8534771773cae70dcfed83e67d407
Andrew Burns <andrewburns43214@gmail.com>
2026-01-27 20:44:27 -0500

    metrics: Step 1E use now_ts for STRONG72_ENTER/EXIT rows (fix ts undefined)

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:53fc3de02b02ce587d435bd8f56fbfd475ad85df29d85dff9f14e0b5d3aad17a
-size 210122
+oid sha256:7173083a65a1588ecfe311a8e19db796890e8439107b0081de1670739598d042
+size 210126
