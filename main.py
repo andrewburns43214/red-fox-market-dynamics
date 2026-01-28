0eee9a15aedfed02642fa3fb7309458013816682
Andrew Burns <andrewburns43214@gmail.com>
2026-01-27 20:27:56 -0500

    metrics: STEP 1F lock signal_ledger writer to 17 cols (ignore extras)

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21c19575fafebac21b6a0f804295f4adcf8c1e9c8c9a7d2b055e693bbc80575b
-size 208549
+oid sha256:b94bf24d7c492b0aae5d6661fc6331fffee9599bd9d5161fcee1bba3af46aa9d
+size 208570
