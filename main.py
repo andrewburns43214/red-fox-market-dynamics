deab97522fd0e660cbf34428108c2f1034c9d6a1
Andrew Burns <andrewburns43214@gmail.com>
2026-01-27 22:41:30 -0500

    metrics: derive strong72_state from last_score to match streak (fix drift)

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:435e9a4334fe59b0db37467365db65a887dc1a6d3a1816a2c95f8ada5ff9f195
-size 208695
+oid sha256:49367779a1ea09456f515368022253c6fa19b39ac4e621a925b5c18e99b4ead0
+size 208910
