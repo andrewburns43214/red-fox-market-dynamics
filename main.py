7338eeeca4838d55e4b50d1a22a5b622172b93c0
Andrew Burns <andrewburns43214@gmail.com>
2026-01-27 22:51:16 -0500

    v1.1: write *_persist_ok/*_stable_ok to dashboard.csv in always-run metrics block

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:405992d133c5faab0c1b375900d1daf4c96acf59ebd4960b851f944b0c0760b6
-size 207335
+oid sha256:5b32e82bf4d6c0bb7ffc7d75c8900a4646754124696467892b16f7a52b50fad3
+size 219488
