bc8275cdc9425c04f077eabef3ce48b600a49fe7
Andrew Burns <andrewburns43214@gmail.com>
2026-01-27 22:58:20 -0500

    chore: make DISABLED_DEAD_GATINGCOLS block raw to remove SyntaxWarning

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af5cf60732b3fce0ce17ae4fdc861d2a4a8c38fc57afe9e1a3a593382c9c9654
-size 219606
+oid sha256:b1fc699e6090f3b8f66900db93acd927524041757a028bf795fe254cd4c6112d
+size 219607
