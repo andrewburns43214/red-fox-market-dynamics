b37a5f5618009c25ecdc1d525f9ef2b35508306a
Andrew Burns <andrewburns43214@gmail.com>
2026-01-27 22:43:55 -0500

    v1.1: STRONG persistence gate uses row_state streak (_prev72), req=2 (NCAAB req=3)

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49367779a1ea09456f515368022253c6fa19b39ac4e621a925b5c18e99b4ead0
-size 208910
+oid sha256:405992d133c5faab0c1b375900d1daf4c96acf59ebd4960b851f944b0c0760b6
+size 207335
