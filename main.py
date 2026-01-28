b653b4abe29947696df230dadcb51f754f9f25ae
Andrew Burns <andrewburns43214@gmail.com>
2026-01-27 20:39:57 -0500

    metrics: remove _default_cols path; hard-lock signal_ledger schema (17 cols)

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b1f5978768d37664bb3d3d351385449fa140d8fd785dae7b913fde3dbe41d19
-size 208648
+oid sha256:49ec5caf95ee58ff172e63d1506f0dce70c1cffd78c37a686f910059f13bcc75
+size 208820
